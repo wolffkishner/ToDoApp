272cca5e90ac110a3a7d60b90c5e639d70cb2d5c
Yashraj <yashrajpahwa@gmail.com>
2020-09-22 23:47:47 +0530

    commit

--- static/js/jqueryFnSet.js
+++ static/js/jqueryFnSet.js
@@ -2,7 +2,12 @@ $(document).ready(function () {
     $('#sidenavToggler').click(function () {
         $('.sidenav').toggleClass('showNav');
         $("i").toggleClass("fa-times");
-        $(".navToggle").toggleClass("navToggleActive")
+        $(".navToggle").toggleClass("navToggleActive");
     });
     $('#datePicker').datepicker({ dateFormat: 'yy-mm-dd' });
+    $('.sidenav a').click(function () {
+        $('.sidenav').toggleClass('showNav');
+        $("i").toggleClass("fa-times");
+        $(".navToggle").toggleClass("navToggleActive");
+    });
 });
